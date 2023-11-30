f8b9431d1a16ca5733d57d91a5dbc989076f1cd7
Prashantk0997 <Prashantk0997@gmail.com>
2023-11-30 21:56:39 +0530

    added

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { useContext , useEffect} from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is about Page
+    </div>
+  )
+}
+
+export default About
